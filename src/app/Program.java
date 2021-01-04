dd10be93a503505eaf21a696a335cf0a8ab4e806
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-01-04 16:04:47 -0300

    dlvr
    dlvr

--- src/app/Program.java
+++ src/app/Program.java
@@ -9,6 +9,8 @@ import db.DB;
 import entities.Order;
 import entities.OrderStatus;
 import entities.Product;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Program {
 
@@ -18,19 +20,41 @@ public class Program {
 	
 		Statement st = conn.createStatement();
 			
-		ResultSet rs = st.executeQuery("select * from tb_order");
-			
+		ResultSet rs = st.executeQuery("SELECT * FROM tb_order\n" +
+"INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id\n" +
+"INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id");
+                
+                Map<Long, Order> map = new HashMap();
+                Map<Long, Product> prods = new HashMap();
 		while (rs.next()) {
                     
-                    Order order = instantiateOrder(rs);
+                    Long orderId = rs.getLong("order_id");
+                    if (map.get(orderId) == null) {
+                        Order order = instantiateOrder(rs);
+                        map.put(orderId, order);
+                    }
                     
-			System.out.println(order);
+                    Long productId = rs.getLong("product_id");
+                    if (prods.get(productId) == null) {
+                        Product p = instantiateProduct(rs);
+                        prods.put(productId, p);
+                    }
+                    
+                    map.get(orderId).getProducts().add(prods.get(productId));                                        
+                }
+                
+                for(Long orderId : map.keySet()) {
+                        System.out.println(map.get(orderId));
+                        for (Product p : map.get(orderId).getProducts()) {
+                            System.out.println(p);
+                        }
+                        System.out.println();
 		}
 	}
         
         private static Order instantiateOrder(ResultSet rs) throws SQLException {
             Order order = new Order();
-            order.setId(rs.getLong("id"));
+            order.setId(rs.getLong("order_id"));
             order.setLatitude(rs.getDouble("latitude"));
             order.setLongitude(rs.getDouble("longitude"));
             order.setMoment(rs.getTimestamp("moment").toInstant());
@@ -40,7 +64,7 @@ public class Program {
         
         private static Product instantiateProduct(ResultSet rs) throws SQLException {
             Product p = new Product();
-            p.setId(rs.getLong("Id"));
+            p.setId(rs.getLong("product_Id"));
             p.setDescription(rs.getString("description"));
             p.setName(rs.getString("name"));
             p.setImageUri(rs.getString("image_uri"));
