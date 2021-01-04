918fb1051f201179edc8656810cc3c53ee8ddc40
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-01-04 14:23:48 -0300

    dlvr
    dlvr

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,19 +6,39 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Connection conn = DB.getConnection();
+                Connection conn = DB.getConnection();
 	
 		Statement st = conn.createStatement();
 			
 		ResultSet rs = st.executeQuery("select * from tb_product");
 			
 		while (rs.next()) {
-			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+                    
+                    Product p = new Product();
+                    p.setId(rs.getLong("Id"));
+                    p.setDescription(rs.getString("description"));
+                    p.setName(rs.getString("name"));
+                    p.setImageUri(rs.getString("image_uri"));
+                    p.setPrice(rs.getDouble("price"));
+                    
+			System.out.println(p);
 		}
 	}
+        
+        private static Product instantiateProduct(ResultSet rs) throws SQLException {
+            Product p = new Product();
+            p.setId(rs.getLong("Id"));
+            p.setDescription(rs.getString("description"));
+            p.setName(rs.getString("name"));
+            p.setImageUri(rs.getString("image_uri"));
+            p.setPrice(rs.getDouble("price"));
+            return p;
+        }
+        
 }
